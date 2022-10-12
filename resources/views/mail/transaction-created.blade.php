8edb9b7344bdb94cc45d99f439b7ec1d1568d184
Tarmizi Sanusi <realtarmizisanusi@gmail.com>
2022-10-12 15:32:24 +0800

    adding mail template

--- resources/views/mail/transaction-created.blade.php
+++ resources/views/mail/transaction-created.blade.php
@@ -1,1 +1,4 @@
-<h1> This is email for transaction created</h1>
+<h1> This is email for transaction created by {{ $transaction->user->name }}</h1>
+<br>
+<h7>Title: {{ $transaction->title }}</h7>
+<h7>Amount: {{ $transaction->amount }}</h7>
