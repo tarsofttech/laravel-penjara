01903131355d430a86deba4a3f7448e17540d94c
Tarmizi Sanusi <realtarmizisanusi@gmail.com>
2022-10-12 11:56:59 +0800

    add middleware

--- routes/web.php
+++ routes/web.php
@@ -22,22 +22,22 @@ Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 // index transaction
-Route::get('/transactions', [App\Http\Controllers\TransactionController::class, 'index'])->name('transactions:index');
+Route::get('/transactions', [App\Http\Controllers\TransactionController::class, 'index'])->name('transactions:index')->middleware('auth');
 
 // create transaction
-Route::get('/transactions/create', [App\Http\Controllers\TransactionController::class, 'create'])->name('transactions:create');
+Route::get('/transactions/create', [App\Http\Controllers\TransactionController::class, 'create'])->name('transactions:create')->middleware('auth');
 
 // store transaction
-Route::post('/transactions/create', [App\Http\Controllers\TransactionController::class, 'store'])->name('transactions:store');
+Route::post('/transactions/create', [App\Http\Controllers\TransactionController::class, 'store'])->name('transactions:store')->middleware('auth');
 
 // show transaction
-Route::get('/transactions/{transaction}', [App\Http\Controllers\TransactionController::class, 'show'])->name('transactions:show');
+Route::get('/transactions/{transaction}', [App\Http\Controllers\TransactionController::class, 'show'])->name('transactions:show')->middleware('auth');
 
 // edit transaction
-Route::get('/transactions/{transaction}/edit', [App\Http\Controllers\TransactionController::class, 'edit'])->name('transactions:edit');
+Route::get('/transactions/{transaction}/edit', [App\Http\Controllers\TransactionController::class, 'edit'])->name('transactions:edit')->middleware('auth');
 
 // update transaction
-Route::post('/transactions/{transaction}/edit', [App\Http\Controllers\TransactionController::class, 'update'])->name('transactions:update');
+Route::post('/transactions/{transaction}/edit', [App\Http\Controllers\TransactionController::class, 'update'])->name('transactions:update')->middleware('auth');
 
 // delete transaction
-Route::get('/transactions/{transaction}/delete', [App\Http\Controllers\TransactionController::class, 'delete'])->name('transactions:delete');
+Route::get('/transactions/{transaction}/delete', [App\Http\Controllers\TransactionController::class, 'delete'])->name('transactions:delete')->middleware('auth');
